9cdfae37bdb6420ea59d9db67e46988ad3e20ef9
JuhoL <juho.lepisto@gmail.com>
2017-03-11 16:08:21 +0200

    Added benchmark.py
    Benchmark.py is a snippet that is used to measure execution time.

---
+++ benchmark.py
@@ -0,0 +1,30 @@
+# Paul McGuire's timestamp solution. Copy-paste from Stack Overflow:
+# http://stackoverflow.com/questions/1557571/how-to-get-time-of-a-python-program-execution
+
+import atexit
+from time import clock
+from datetime import timedelta
+
+def secondsToStr(t):
+    return str(timedelta(seconds=t))
+
+line = "="*40
+def log(s, elapsed=None):
+    print(line)
+    print(secondsToStr(clock()), '-', s)
+    if elapsed:
+        print("Elapsed time:", elapsed)
+    print(line)
+    print()
+
+def endlog():
+    end = clock()
+    elapsed = end-start
+    log("End Program", secondsToStr(elapsed))
+
+def now():
+    return secondsToStr(clock())
+
+start = clock()
+atexit.register(endlog)
+log("Start Program")
